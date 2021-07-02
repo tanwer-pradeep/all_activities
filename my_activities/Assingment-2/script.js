415905279c052990dd0dd13dfa2169df0d7d8a4d
tanwer-pradeep <pradeepsinghonline@gmail.com>
2021-07-02 20:05:24 +0530

    changed

--- my_activities/Assingment-2/script.js
+++ my_activities/Assingment-2/script.js
@@ -1,12 +1,6 @@
 let showing = document.querySelector("#current-city");
 let relocation = document.querySelector("#relocation");
-// console.log(showing);
-// showing.addEventListener("click", ()=>{
-//     console.log(1)
-//     let value = showing.getAttribute("value");
-//     console.log(value);
 
-// })
 showing.addEventListener("change", ()=>{
     console.log(showing.value);
     if(showing.value === "Others"){
@@ -15,4 +9,3 @@ showing.addEventListener("change", ()=>{
     }else relocation.setAttribute("hidden", true);
 })
 
-// console.log(relocation);
