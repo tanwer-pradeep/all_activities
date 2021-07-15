ba0f44dda0fc5d70998d3a8300b989a5e9fa6220
tanwer-pradeep <pradeepsinghonline@gmail.com>
2021-07-15 23:41:00 +0530

    update

--- my_activities/bookmark/script.js
+++ my_activities/bookmark/script.js
@@ -1,1 +1,20 @@
-console.log("done");
+const addmodal = document.getElementById('head');
+const modal = document.getElementById('modal');
+const closemodal = document.getElementById('close-modal');
+// console.log(closemodal);
+let showmodal = false;
+
+addmodal.addEventListener('click', () =>{
+    modal.classList.add('show');
+    showmodal = true
+})
+
+closemodal.addEventListener('click', () =>{
+    modal.classList.remove('show');
+})
+
+document.addEventListener('click', (e) =>{
+    if(showmodal == true && e.target == 'div#modal.modal-container.show')modal.classList.remove('show');
+    console.log(e.target);
+    console.log(showmodal);
+})
